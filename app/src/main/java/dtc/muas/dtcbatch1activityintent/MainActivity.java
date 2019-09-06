a09ad0a93450a2072357ce626b753781d3a64db5
muasmakkode <muasstepa7@gmail.com>
2019-09-06 22:59:21 +0700

    update

--- app/src/main/java/dtc/muas/dtcbatch1activityintent/MainActivity.java
+++ app/src/main/java/dtc/muas/dtcbatch1activityintent/MainActivity.java
@@ -3,6 +3,7 @@ package dtc.muas.dtcbatch1activityintent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,12 +16,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnPindahActivity = findViewById(R.id.btnPindahActivity);
+        Button btnPindahActivityDenganData = findViewById(R.id.btnPindahActivityDenganData);
+        Button btnTelepon = findViewById(R.id.btnTelepon);
+        Button btnBukaBrowser = findViewById(R.id.btnBukaBrowser);
 
         btnPindahActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(intent);
+            }
+        });
+        btnPindahActivityDenganData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("NAMA", "Muasdev");
+                intent.putExtra("ASAL", "Parepare");
+                intent.putExtra("PEKERJAAN", "Android Dev");
+                startActivity(intent);
+            }
+        });
+        btnTelepon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNumber = "082347218036";
+                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+                startActivity(dialPhoneIntent);
+            }
+        });
+        btnBukaBrowser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String alamatWebsite = "http://www.google.com";
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(alamatWebsite));
+                if (browserIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(browserIntent);
+                }
             }
         });
     }
--- app/src/main/java/dtc/muas/dtcbatch1activityintent/SecondActivity.java
+++ app/src/main/java/dtc/muas/dtcbatch1activityintent/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -12,5 +13,16 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        TextView tvNama = findViewById(R.id.tvNama);
+        TextView tvAsal = findViewById(R.id.tvAsal);
+        TextView tvPekerjaan = findViewById(R.id.tvPekerjaan);
+
+        String nama = getIntent().getStringExtra("NAMA");
+        String asal = getIntent().getStringExtra("ASAL");
+        String pekerjaan = getIntent().getStringExtra("PEKERJAAN");
+
+        tvNama.setText(nama);
+        tvAsal.setText(asal);
+        tvPekerjaan.setText(pekerjaan);
     }
 }
