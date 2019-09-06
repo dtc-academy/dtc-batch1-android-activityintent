be723a29e4816c089042b07e4df6621f11a89965
muasmakkode <muasstepa7@gmail.com>
2019-09-06 22:10:16 +0700

    init

---
+++ app/src/main/java/dtc/muas/dtcbatch1activityintent/SecondActivity.java
@@ -0,0 +1,16 @@
+package dtc.muas.dtcbatch1activityintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
